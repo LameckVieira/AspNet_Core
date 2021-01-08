a7f1dbc9506bdf387e1f142c8812987c277ad056
LameckVieira <lameck.v.barbosa@gmail.com>
2021-01-08 09:01:25 -0300

    MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNet_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
